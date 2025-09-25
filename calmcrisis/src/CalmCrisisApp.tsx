7883c22c9a5d2a9bd4f8138ef0fd582eff043fd1
s.ribnovo@abv.bg <s.ribnovo@abv.bg>
2025-09-25 19:41:17 +0300

    Add modal recomender

--- calmcrisis/src/CalmCrisisApp.tsx
+++ calmcrisis/src/CalmCrisisApp.tsx
@@ -825,7 +825,7 @@ function Recommend({ currentContext }: { currentContext: Episode["context"] }) {
                             </div>
                         </div>
                         <button
-                            className="px-3 py-2 rounded-xl bg-blue-600 text-white hover:bg-blue-700"
+                            //className="px-3 py-2 rounded-xl bg-blue-600 text-white hover:bg-blue-700"
                             onClick={() => setActive({ name: r.name })}
                         >
                             {t("start")}
